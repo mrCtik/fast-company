7a90f0da147133146996e94444d660d06237278b
mrCtik <99313450+mrCtik@users.noreply.github.com>
2022-11-09 20:42:50 +0700

    lesson 20 complited

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -4,7 +4,6 @@ import UserCard from "../../ui/userCard";
 import QualitiesCard from "../../ui/qualitiesCard";
 import MeetingsCard from "../../ui/meetingsCard";
 import Comments from "../../ui/comments";
-import { CommentsProvider } from "../../../hooks/useComments";
 import { getUserById } from "../../../store/users";
 import { useSelector } from "react-redux";
 
@@ -21,9 +20,7 @@ const UserPage = ({ userId }) => {
                         <MeetingsCard value={user.completedMeetings} />
                     </div>
                     <div className="col-md-8">
-                        <CommentsProvider>
-                            <Comments />
-                        </CommentsProvider>
+                        <Comments />
                     </div>
                 </div>
             </div>
